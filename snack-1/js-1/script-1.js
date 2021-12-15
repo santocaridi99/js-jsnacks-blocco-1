109398c255f68e971c85033fd250ab62adcc9ef6
santocaridi99 <santocaridi99@gmail.com>
2021-12-15 18:41:43 +0100

    (snack1)aggiunta bonus di una variabile number per visualizzare i numeri inseriti

--- snack-1/js-1/script-1.js
+++ snack-1/js-1/script-1.js
@@ -1,9 +1,12 @@
 //dichiaro una variabile somma a 0 che mi servirà per avere la somma dei numeri
 let sum = 0;
+let number;
 //ciclo for da 0  a 10, dove dentro avrò il prompt che chiederà di inserire un numero
 for(let i = 0 ; i < 10 ;i++){
     //sum=sum+numeroinserito così per 10 volte 
-    sum+= parseInt(prompt('inserisci un numero'));
+    number=parseInt(prompt('inserisci un numero'));
+    sum+=number;
+    console.log(number);
 }
 //stampo
 console.log(sum);
