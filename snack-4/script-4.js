16d98df42ba266b34b519a899e682b68c3f94572
santocaridi99 <santocaridi99@gmail.com>
2021-12-16 10:23:08 +0100

    snack(4) dovevo inserire un numero di 4 cifre non massimo 4 cifre

--- snack-4/script-4.js
+++ snack-4/script-4.js
@@ -2,8 +2,8 @@
 const numero = prompt('inserisci il tuo numero(max 4 cifre):');
 //una variabile sum che mi servirà per fare la somma delle cifre
 sum=0;
-//se lunghezza delle cifre è < 4 allora 
-if(numero.length<=4){
+//se lunghezza delle cifre è === 4 allora 
+if(numero.length===4){
     //faccio un for da 0 a numero cifre
     for(let i=0 ; i<numero.length;i++){
         //sum=sum+indice che ha controllato la lunghezza sella stringa (lo trasformiamo in intero)
@@ -13,5 +13,5 @@ if(numero.length<=4){
     console.log(sum);
     //altrimenti warning
 }else{
-    console.log('attenzione numero supera le 4 cifre')
+    console.log('attenzione non è di  4 cifre')
 }
